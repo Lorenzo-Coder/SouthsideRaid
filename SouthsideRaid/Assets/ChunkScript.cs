4bf38984fa28bf51f294deb0648a3240366ea316
jlu787 <jlu787@outlook.com>
2019-09-02 19:28:03 +1200

    Finished the HealthBar Juice
    Added prefab for chunk
    Added prefab for new HP bar
    Added new script HPBarScript which spawns new chunks
    Added new script ChunkScript which deletes Chunks after a few seconds

---
+++ SouthsideRaid/Assets/ChunkScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkScript : MonoBehaviour
+{
+
+    IEnumerator CleanUp()
+    {
+        yield return new WaitForSeconds(3.0f);
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CleanUp());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- SouthsideRaid/Assets/Scripts/HPBarScript.cs
+++ SouthsideRaid/Assets/Scripts/HPBarScript.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class HPBarScript : MonoBehaviour
 {
     public Image fill;
     public Image chunk;
+    public Color endColor;
     GameObject boss;
     // Start is called before the first frame update
     void Start()
     {
         boss = GameObject.FindGameObjectWithTag("Boss");
+    }
+
+    IEnumerator CleanUp(Image _chunk)
+    {
+        yield return new WaitForSeconds(3.0f);
+        Destroy(_chunk);
     }
 
     // Update is called once per frame
@@ -20,13 +28,28 @@ public class HPBarScript : MonoBehaviour
         
     }
 
+    // this is called whenever boss takes damage
     public void RemoveChunk(int _damage)
     {
         // get the scale
-        float scaleX = _damage / boss.GetComponent<BossScript>().maxHealth;
-        Debug.Log("new chunk");
-        Image newChunk = Instantiate(chunk, boss.GetComponent<Canvas>().transform);
+        float scaleX = (float)_damage / (float)boss.GetComponent<BossScript>().maxHealth;
+
+        // get the scale for the position on the x axis
+        float posScale = (float)boss.GetComponent<BossScript>().currentHealth /(float)boss.GetComponent<BossScript>().maxHealth;
+
+        // create the new chunk with the canvas as parent
+        Image newChunk = Instantiate(chunk, boss.GetComponentInChildren<Canvas>().transform);
+
+        // set the new scale and positions
+        float newPos = fill.rectTransform.position.x + (posScale * fill.rectTransform.rect.width);
+        newChunk.rectTransform.anchoredPosition3D = new Vector3(newChunk.rectTransform.anchoredPosition3D.x + posScale * fill.rectTransform.rect.width, newChunk.rectTransform.anchoredPosition3D.y, fill.transform.position.z);
         newChunk.transform.localScale = new Vector3(scaleX, newChunk.transform.localScale.y, newChunk.transform.localScale.z);
-        newChunk.transform.position = new Vector3(fill.transform.position.x + (scaleX * fill.rectTransform.rect.width), fill.transform.position.y, fill.transform.position.z);
+
+        // fade the chunk
+        newChunk.GetComponent<Image>().DOColor(endColor, 0.5f);
+
+        // move the chunk slightly
+        newChunk.transform.DOLocalMoveY(20, 1.0f, false);
+        newChunk.transform.DOLocalMoveX(newChunk.rectTransform.anchoredPosition3D.x + 5, 0.5f, false);
     }
 }
