0a200ea13acfa55e6606cd195d22fbdfadd6eb84
Jason <jlu787@outlook.com>
2019-09-04 18:35:28 +1200

    Added audio files

---
+++ SouthsideRaid/Assets/TestScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+    public GameObject playerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            GameObject newPlayer = Instantiate(playerPrefab);
+            //newPlayer.GetComponent<PlayerScript>().JoinGame();
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GameObject[] playerList = GameObject.FindGameObjectsWithTag("Player");
+            for(int i = 0; i < playerList.Length; i++)
+            {
+                playerList[i].GetComponent<PlayerScript>().JoinGame();
+            }
+        }
+    }
+}
