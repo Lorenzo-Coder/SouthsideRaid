fb817710ab9c8e23e64ccbf48d1b008b840ad043
Jack <jack.belton@hotmail.com>
2019-10-30 16:48:08 +1300

    New sprites imported

--- SouthsideRaid/Assets/Scripts/LeaderboardScript.cs
+++ SouthsideRaid/Assets/Scripts/LeaderboardScript.cs
@@ -7,28 +7,25 @@ using DG.Tweening;
 public class ScoreSet
 {
     public GameObject AttachedPlayer;
-    public Text AttachedText;
+    public GameObject AttachedTextBox;
+
     public int ScoreEasyAccess = 0;
     public void UpdateText()
     { //joinedList[i].GetComponent<PlayerScript>().playerName + "  " + joinedList[i].GetComponent<PlayerScript>().playerScore
-        AttachedText.text = AttachedPlayer.GetComponent<PlayerScript>().playerName + "  " + AttachedPlayer.GetComponent<PlayerScript>().playerScore;
         ScoreEasyAccess = AttachedPlayer.GetComponent<PlayerScript>().playerScore;
+        AttachedTextBox.transform.GetChild(0).GetComponent<Text>().text = AttachedPlayer.GetComponent<PlayerScript>().playerName + "  " + AttachedPlayer.GetComponent<PlayerScript>().playerScore;
     }
 }
 
 public class LeaderboardScript : MonoBehaviour
 {
-    public Text leaderboardPos1;
-    public Text leaderboardPos2;
-    public Text leaderboardPos3;
-    public Text leaderboardPos4;
+    public GameObject[] leaderboardArray;
 
     private Vector3[] originalPos;
     private Vector3[] originalScale;
 
     private List<ScoreSet> HighScoreSet;
 
-    private Text[] leaderboardArray;
     private int showTop = 4;
 
     public Text monsterLevelText;
@@ -47,38 +44,20 @@ public class LeaderboardScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        leaderboardArray = new Text[4];
-        leaderboardArray[0] = leaderboardPos1;
-        leaderboardArray[1] = leaderboardPos2;
-        leaderboardArray[2] = leaderboardPos3;
-        leaderboardArray[3] = leaderboardPos4;
-
-        
         HighScoreSet = new List<ScoreSet>();
-        GameObject[] getPlayers = GameObject.FindGameObjectsWithTag("Player");
-
-        for (int i = 0; i < showTop; i++)
-        {
-            ScoreSet tempSet = new ScoreSet();
-            tempSet.AttachedPlayer = getPlayers[i];
-            tempSet.AttachedText = leaderboardArray[i];
-            tempSet.AttachedText.gameObject.SetActive(false);
-            tempSet.AttachedText.text = "";
-
-            HighScoreSet.Add(tempSet);
-        }
-
         originalPos = new Vector3[4];
-        originalScale = new Vector3[4];
-        originalPos[0] = leaderboardPos1.transform.localPosition;
-        originalScale[0] = leaderboardPos1.transform.localScale;
-        originalPos[1] = leaderboardPos2.transform.localPosition;
-        originalScale[2] = leaderboardPos2.transform.localScale;
-        originalPos[2] = leaderboardPos3.transform.localPosition;
-        originalScale[3] = leaderboardPos3.transform.localScale;
-        originalPos[3] = leaderboardPos4.transform.localPosition;
-        //originalScale[4] = leaderboardPos4.transform.localScale;
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Player");
+        for (int i = 0; i < leaderboardArray.Length; i++)
+        {
+            originalPos[i] = leaderboardArray[i].transform.localPosition;
 
+            ScoreSet scoreSet = new ScoreSet();
+            scoreSet.AttachedTextBox = leaderboardArray[i];
+            scoreSet.AttachedPlayer = objects[i];
+
+            HighScoreSet.Add(scoreSet);
+
+        }
     }
 
     private List<GameObject> oldJoined = new List<GameObject>();
@@ -92,12 +71,11 @@ public class LeaderboardScript : MonoBehaviour
         {
             // check if any of them have joined
             int joined = 0;
-            //List<GameObject> joinedList = new List<GameObject>();
             for (int i = 0; i < HighScoreSet.Count; i++)
             {
                 if (HighScoreSet[i].AttachedPlayer.GetComponent<PlayerScript>().joined)
                 {
-                    HighScoreSet[i].AttachedText.gameObject.SetActive(true);
+                    HighScoreSet[i].AttachedTextBox.SetActive(true);
                     joined++;
                 }
             }
@@ -115,8 +93,7 @@ public class LeaderboardScript : MonoBehaviour
 
             for (int i = 0; i < HighScoreSet.Count; i++)
             {
-                HighScoreSet[i].AttachedText.transform.DOLocalMove(originalPos[i], 0.2f);
-                //HighScoreSet[i].AttachedText.transform.DOScale(originalScale[i], 0.2f);
+                HighScoreSet[i].AttachedTextBox.transform.DOLocalMove(originalPos[i], 0.2f);
             }
         }
 
