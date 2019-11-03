2f730e22645fe829282819f7ec042f81242d162c
Lorenzo Sae-Phoo Zemp <lorenzo.zem8065@mediadesign.school.nz>
2019-11-03 22:18:03 +1300

    I am braindead gimme a sec

---
+++ SouthsideRaid/Assets/attackPrefabScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attackPrefabScript : MonoBehaviour
+{
+    public float timeToDamage = 0.5f;
+    public float blinkRate = 0.08f;
+    public int howManyBlink = 10;
+
+    private GameObject boss;
+    private Renderer objectRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectRenderer = gameObject.GetComponent<Renderer>();
+        boss = GameObject.FindGameObjectWithTag("Boss");
+        //InvokeRepeating("blink", 0.0f, 1.0f);
+        StartCoroutine(blink());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    IEnumerator blink()
+    {
+        for (int i = 0; i < howManyBlink; i++)
+        {
+            objectRenderer.enabled = false;
+            yield return new WaitForSeconds(blinkRate);
+            objectRenderer.enabled = true;
+            Debug.Log("offnon");
+        }
+    }
+}
