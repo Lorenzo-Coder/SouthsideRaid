5d4e69b3beb749e824098b7e85334efceea42c14
Lorenzo Sae-Phoo Zemp <lorenzo.zem8065@mediadesign.school.nz>
2019-11-05 22:39:02 +1300

    Progress on attack prefab

--- SouthsideRaid/Assets/attackPrefabScript.cs
+++ SouthsideRaid/Assets/attackPrefabScript.cs
@@ -5,34 +5,81 @@ using UnityEngine;
 public class attackPrefabScript : MonoBehaviour
 {
     public float timeToDamage = 0.5f;
-    public float blinkRate = 0.08f;
+    public float blinkRate  = 1.0f;
     public int howManyBlink = 10;
 
     private GameObject boss;
-    private Renderer objectRenderer;
+    private SpriteRenderer spriteRenderer;
+    private Color defaultColor;
+    private bool visible = true;
+    private bool countdownEnded = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        objectRenderer = gameObject.GetComponent<Renderer>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        defaultColor = spriteRenderer.color;
+
         boss = GameObject.FindGameObjectWithTag("Boss");
-        //InvokeRepeating("blink", 0.0f, 1.0f);
-        StartCoroutine(blink());
+        //StartCoroutine(blink());
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        //if (countdownEnded == false)
+        //{
+            timeToDamage -= Time.deltaTime;
+            Debug.Log(timeToDamage);
+            if (timeToDamage <= 0.0f)
+            {
+                countdownEnded = true;
+            }
+        //}
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("help me im crying");
+        if ((other.tag == "Player") && (countdownEnded == true))
+        {
+            Debug.Log("player in trigger, countrdown ended");
+            if (other.GetComponent<PlayerScript>().invincible == false)
+            {
+                other.GetComponent<PlayerScript>().GetHit();
+                Debug.Log("called player getHit");
+            }
+        }
     }
 
     IEnumerator blink()
     {
-        for (int i = 0; i < howManyBlink; i++)
+        for (int i = 0; i < howManyBlink * 2; i++)
         {
-            objectRenderer.enabled = false;
+            //objectRenderer.enabled = false;
+            //objectRenderer.enabled = true;
+            //if (gameObject.activeSelf)
+            //{
+            //    gameObject.SetActive(false);
+            //}
+            //else
+            //{
+            //    gameObject.SetActive(true);
+            //}
+            if(visible)
+            {
+                spriteRenderer.color = new Color (defaultColor.r, defaultColor.g, defaultColor.b, 0);
+                Debug.Log("off");
+                visible = false;
+            }
+            else
+            {
+                spriteRenderer.color = defaultColor;
+                Debug.Log("on");
+                visible = true;
+            }
             yield return new WaitForSeconds(blinkRate);
-            objectRenderer.enabled = true;
-            Debug.Log("offnon");
         }
+        spriteRenderer.color = new Color(defaultColor.r, defaultColor.g, defaultColor.b, 0);
     }
 }
